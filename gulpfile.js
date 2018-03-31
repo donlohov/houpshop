cf8f7b4413d15db0d90885f8c8a7217d16387ea7
donlohov <donlohov@gmail.com>
2018-04-01 01:27:20 +0700

    fixed bugs

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,20 @@
 'use strict';
 
-var gulp = require('gulp');
-    concat = require('gulp-concat');
-    minify = require('gulp-minify');
-    autoprefixer = require('gulp-autoprefixer');
-    del = require('del');
-    imagemin = require('gulp-imagemin');
-    plumber = require('gulp-plumber');
-    rename = require('gulp-rename');
-    sourcemaps = require('gulp-sourcemaps');
-    stylus = require('gulp-stylus');
-    pngquant = require('imagemin-pngquant');
-    pug = require('gulp-pug');
-    uglify = require('gulp-uglify');
+var gulp = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    concat = require('gulp-concat'),
+    csso = require('gulp-csso'),
+    del = require('del'),
+    imagemin = require('gulp-imagemin'),
+    plumber = require('gulp-plumber'),
+    rename = require('gulp-rename'),
+    sourcemaps = require('gulp-sourcemaps'),
+    stylus = require('gulp-stylus'),
+    pngquant = require('imagemin-pngquant'),
+    pug = require('gulp-pug'),
+    uglify = require('gulp-uglify')
 
-var bruwserSync = require('browser-sync').create(),
+var browserSync = require('browser-sync').create(),
     reload = browserSync.reload;
 
 gulp.task('html', function(){
